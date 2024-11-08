f8e1d3f60bacd749a0f4f7d490e0ba2d7eb26fae
crushed-taro <97sinsang@gmail.com>
2024-11-08 11:20:49 +0900

    Spring Boot Server First Commit

---
+++ travelanchor_rest_server/src/main/java/travelanchor_server/TravelanchorRestServerApplication.java
@@ -0,0 +1,13 @@
+package travelanchor_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelanchorRestServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelanchorRestServerApplication.class, args);
+    }
+
+}
---
+++ travelanchor_rest_server/src/test/java/travelanchor_server/TravelanchorRestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package travelanchor_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelanchorRestServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
