2e5e53f4deec9b3829b0249da2a2fc5d8963ff52
JingiHong <hjg0729@naver.com>
2024-11-12 23:52:09 +0900

    JWT, Security, sql문 테이블 추가 및 회원 정보 수정

--- travelanchor_rest_server/src/main/java/travelanchor_server/config/SecurityConfig.java
+++ travelanchor_rest_server/src/main/java/travelanchor_server/config/SecurityConfig.java
@@ -1,16 +1,24 @@
 package travelanchor_server.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import travelanchor_server.jwt.JwtAccessDeniedHandler;
+import travelanchor_server.jwt.JwtAuthenticationEntryPoint;
+import travelanchor_server.jwt.JwtFilter;
+import travelanchor_server.jwt.TokenProvider;
 
 import java.util.Arrays;
 
@@ -18,14 +26,71 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    // JWT 토큰을 발급하고 검증하는 Token Provider
+    private final TokenProvider tokenProvider;
+    // 인증 실패 관련 예외
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    // 접근 거부 관련 예외
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Autowired
+    public SecurityConfig(TokenProvider tokenProvider,
+                          JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
+                          JwtAccessDeniedHandler jwtAccessDeniedHandler) {
+        this.tokenProvider = tokenProvider;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+    }
+
+    /* 목차. 1. 암호화 처리를 위한 PasswordEncoder를 빈으로 설정(빈을 등록 시 메소드 이름 오타 없도록 주의!) */
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /* 목차. 2. Spring Security 설정을 무시 할 정적 리소스 등록 */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .requestMatchers("/css/**", "/js/**", "/images/**", "/lib/**", "/productimgs/**");
+    }
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable() // CSRF 비활성화 (선택)
-                .authorizeHttpRequests(authz -> authz
-                        .anyRequest().permitAll() // 모든 요청을 허용
-                )
-                .formLogin().disable().cors(cors -> {}); // 로그인 화면 비활성화 (필요에 따라)
+                // 1. CSRF 보호 비활성화
+                .csrf(csrf -> csrf.disable())
+                // 2. 예외 처리
+                .exceptionHandling(exception -> {
+                    // 필요한 권한이 없을 때 403(Forbidden)을 반환
+                    exception.authenticationEntryPoint(jwtAuthenticationEntryPoint);
+                    // 인증되지 않은 접근 시 401(Unauthorized)를 반환
+                    exception.accessDeniedHandler(jwtAccessDeniedHandler);
+                })
+                // 3. HTTP 요청에 대한 접근 권한 설정
+                .authorizeHttpRequests(auth -> {
+                    // CORS Preflight 요청 허용
+                    auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+                    // root 경로는 인증 필요
+                    auth.requestMatchers("/").authenticated();
+                    // 특정 경로는 무조건 허용
+                    auth.requestMatchers("/auth/**", "/api/v1/products/**", "/api/v1/reviews/**").permitAll();
+                    // Swagger API 문서 허용
+                    auth.requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll();
+                    // API 경로는 USER 또는 ADMIN 역할을 가진 사용자만 접근 가능
+                    auth.requestMatchers("/api/**").hasAnyRole("USER", "ADMIN");
+                    // 어떤 요청이든 허용 -> Security를 활용한 로그인이 모두 완성되지 않았을 때 사용할 것
+//                    auth.anyRequest().permitAll();
+                })
+                // 4. 세션 방식을 사용하지 않음
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // 5. 기본 CORS 설정 사용
+                .cors(cors -> {})
+                // 6. 우리가 직접 작성한 커스텀 필터인 JwtFilter를 필터 체인에 추가
+                .addFilterBefore(new JwtFilter(tokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
--- travelanchor_rest_server/src/main/java/travelanchor_server/member/entity/Member.java
+++ travelanchor_rest_server/src/main/java/travelanchor_server/member/entity/Member.java
@@ -1,5 +1,169 @@
 package travelanchor_server.member.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "tbl_member")
 public class Member {
 
+    @Id
+    @Column(name = "member_code")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int memberCode;
+
+    @Column(name = "authority_code")
+    private int authorityCode;
+
+    @Column(name = "member_name")
+    private String memberName;
+
+    @Column(name = "member_nickname")
+    private String memberNickName;
+
+    @Column(name = "member_mobile_number")
+    private String memberMobileNumber;
+
+    @Column(name = "member_created_at")
+    private LocalDate memberCreatedAt;
+
+    @Column(name = "member_id")
+    private String memberId;
+
+    @Column(name = "member_password")
+    private String memberPassword;
+
+    @Column(name = "member_level")
+    private int memberLevel;
+
+    @Column(name = "member_certification")
+    private String memberCertification;
+
+    @OneToMany
+    @JoinColumn(name = "member_code")
+    private List<MemberRole> memberRole;
+
+    public Member() {
+    }
+
+    public Member(int memberCode, int authorityCode, String memberName, String memberNickName, String memberMobileNumber, LocalDate memberCreatedAt, String memberId, String memberPassword, int memberLevel, String memberCertification, List<MemberRole> memberRole) {
+        this.memberCode = memberCode;
+        this.authorityCode = authorityCode;
+        this.memberName = memberName;
+        this.memberNickName = memberNickName;
+        this.memberMobileNumber = memberMobileNumber;
+        this.memberCreatedAt = memberCreatedAt;
+        this.memberId = memberId;
+        this.memberPassword = memberPassword;
+        this.memberLevel = memberLevel;
+        this.memberCertification = memberCertification;
+        this.memberRole = memberRole;
+    }
+
+    public int getMemberCode() {
+        return memberCode;
+    }
+
+    public void setMemberCode(int memberCode) {
+        this.memberCode = memberCode;
+    }
+
+    public int getAuthorityCode() {
+        return authorityCode;
+    }
+
+    public void setAuthorityCode(int authorityCode) {
+        this.authorityCode = authorityCode;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberNickName() {
+        return memberNickName;
+    }
+
+    public void setMemberNickName(String memberNickName) {
+        this.memberNickName = memberNickName;
+    }
+
+    public String getMemberMobileNumber() {
+        return memberMobileNumber;
+    }
+
+    public void setMemberMobileNumber(String memberMobileNumber) {
+        this.memberMobileNumber = memberMobileNumber;
+    }
+
+    public LocalDate getMemberCreatedAt() {
+        return memberCreatedAt;
+    }
+
+    public void setMemberCreatedAt(LocalDate memberCreatedAt) {
+        this.memberCreatedAt = memberCreatedAt;
+    }
+
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(String memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getMemberPassword() {
+        return memberPassword;
+    }
+
+    public void setMemberPassword(String memberPassword) {
+        this.memberPassword = memberPassword;
+    }
+
+    public int getMemberLevel() {
+        return memberLevel;
+    }
+
+    public void setMemberLevel(int memberLevel) {
+        this.memberLevel = memberLevel;
+    }
+
+    public String getMemberCertification() {
+        return memberCertification;
+    }
+
+    public void setMemberCertification(String memberCertification) {
+        this.memberCertification = memberCertification;
+    }
+
+    public List<MemberRole> getMemberRole() {
+        return memberRole;
+    }
+
+    public void setMemberRole(List<MemberRole> memberRole) {
+        this.memberRole = memberRole;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "memberCode=" + memberCode +
+                ", authorityCode=" + authorityCode +
+                ", memberName='" + memberName + '\'' +
+                ", memberNickName='" + memberNickName + '\'' +
+                ", memberMobileNumber='" + memberMobileNumber + '\'' +
+                ", memberCreatedAt=" + memberCreatedAt +
+                ", memberId='" + memberId + '\'' +
+                ", memberPassword='" + memberPassword + '\'' +
+                ", memberLevel=" + memberLevel +
+                ", memberCertification='" + memberCertification + '\'' +
+                ", memberRole=" + memberRole +
+                '}';
+    }
 }
