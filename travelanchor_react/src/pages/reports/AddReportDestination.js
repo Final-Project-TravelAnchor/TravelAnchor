0294d6ca3bef224ded422ea73329dbca7a9880f0
dlskfk <naralee95@gmail.com>
2024-11-12 19:52:26 +0900

    여행후기 및 sql문 수정

---
+++ travelanchor_react/src/pages/reports/AddReportDestination.js
@@ -0,0 +1,16 @@
+function AddReportDestination() {
+
+    return (
+        <div>
+            <h3>목적지를 선택해 주세요</h3>
+            <h3>여행 테마를 선택해 주세요</h3>
+            <div>
+                <ul>
+                    
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default AddReportDestination;
