45a66d3d1b2373b89c30bae39fbcb48def8b6d21
JingiHong <hjg0729@naver.com>
2024-11-13 11:33:57 +0900

    로그인 기능 추가

--- travelanchor_rest_server/src/main/java/travelanchor_server/member/controller/AuthController.java
+++ travelanchor_rest_server/src/main/java/travelanchor_server/member/controller/AuthController.java
@@ -1,12 +1,35 @@
 package travelanchor_server.member.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import travelanchor_server.common.ResponseDTO;
+import travelanchor_server.member.dto.MemberDTO;
+import travelanchor_server.member.service.AuthService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthService authService;
 
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Operation(summary = "로그인 요청", description = "로그인 및 인증이 진행됩니다.", tags = {"AuthController"})
+    @PostMapping("/login")
+    public ResponseEntity<ResponseDTO> login(@RequestBody MemberDTO memberDTO) {
+
+        return ResponseEntity
+                .ok()
+                .body(new ResponseDTO(HttpStatus.OK, "로그인 성공~", authService.login(memberDTO)));
+    }
 }
