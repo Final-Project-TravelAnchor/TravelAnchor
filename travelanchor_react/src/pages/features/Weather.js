1377f94dde42dd473f0731e13dce0585ec685d5a
jieunjieune <jieune120@gmail.com>
2024-11-15 15:35:49 +0900

    날씨 조회 기능 수정

--- travelanchor_react/src/pages/features/Weather.js
+++ travelanchor_react/src/pages/features/Weather.js
@@ -1,64 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import { callOpenWeather } from '../../apis/WeatherAPICalls';
 
-function Weather() {
+export default function Weather() {
 	const [city, setCity] = useState('');
 	const [forecast, setForecast] = useState(null);
 	const [error, setError] = useState('');
 
-	const fetchForecast = () => {
-		if (!city) return;
-
-		fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
-		.then(response => response.json())
-		.then(data => {
-			if (data.cod === "200") {
-			setForecast(data);
-			setError('');
-			} else {
-			setError("도시를 찾을 수 없습니다.");
-			setForecast(null);
-			}
-		})
-		.catch(error => {
-			setError("날씨 정보를 가져오는 중 오류가 발생했습니다.");
-			setForecast(null);
-			console.error("Error:", error);
-		});
+	const fetchForecast = async () => {
+		try {
+			const forecastResponse = await callOpenWeather(city); // city를 인자로 전달
+			console.log("날씨예보 : ", forecastResponse);
+			setForecast(forecastResponse); // 상태 업데이트
+			setError(''); // 오류 초기화
+		} catch (err) {
+			setError(err.message); // 오류 메시지 설정
+			setForecast(null); // 예보 초기화
+		}
 	};
 
 	return (
 		<div className="container">
-		<h1>5-Day Weather Forecast</h1>
-		
-		<input
-			type="text"
-			value={city}
-			onChange={(e) => setCity(e.target.value)}
-			placeholder="도시명을 입력하세요"
-		/>
-		<button onClick={fetchForecast}>날씨 조회</button>
+			<h1>5일간의 Weather Forecast</h1>
+			<input
+				type="text"
+				value={city}
+				onChange={(e) => setCity(e.target.value)}
+				placeholder="도시명을 영문으로 입력하세요 예:seoul"
+			/>
+			<button onClick={fetchForecast}>날씨 조회</button>
 
-		{error && <p>{error}</p>}
+			{error && <p>{error}</p>}
 
-		{forecast && (
-			<div id="forecast">
-			<h2>{forecast.city.name}의 날씨 예보</h2>
-			{forecast.list.slice(0, 40).map((item, index) => {
-				const date = new Date(item.dt * 1000);
-				return (
-				<div key={index}>
-					<h3>{date.toLocaleString()}</h3>
-					<p>온도: {item.main.temp}°C</p>
-					<p>날씨: {item.weather[0].description}</p>
-					<p>풍속: {item.wind.speed} m/s</p>
-					<hr />
+			{forecast && (
+				<div id="forecast">
+					<h2>{forecast.city.name}의 날씨 예보</h2>
+					{forecast.list.slice(0, 40).map((item, index) => {
+						const date = new Date(item.dt * 1000);
+						return (
+							<div key={index}>
+								<h3>{date.toLocaleString()}</h3>
+								<p>온도: {item.main.temp}°C</p>
+								<p>날씨: {item.weather[0].description}</p>
+								<p>풍속: {item.wind.speed} m/s</p>
+								<hr />
+							</div>
+						);
+					})}
 				</div>
-				);
-			})}
-			</div>
-		)}
+			)}
 		</div>
 	);
 }
-
-export default Weather;
