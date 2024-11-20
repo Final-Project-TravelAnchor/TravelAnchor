7080e19f84053d3b73b6cef34e7b0ef5d3cce7f0
jieunjieune <jieune120@gmail.com>
2024-11-20 13:01:17 +0900

    헤더오류 수정

--- travelanchor_react/src/components/common/Header.js
+++ travelanchor_react/src/components/common/Header.js
@@ -156,7 +156,7 @@ function Header() {
 				{/* navbar */}
 				<div>
 					<ul class="navMenu" className={HeaderCSS.navMenu}>
-						<li class="navItem" className={HeaderCSS.navItem}>
+						{/* <li class="navItem" className={HeaderCSS.navItem}>
 							<a href="#">여행</a>
 							<ul class="dropdownMenu" className={HeaderCSS.dropdownMenu}>
 								<li><a href="#">여행지</a></li>
@@ -164,7 +164,7 @@ function Header() {
 								<li><NavLink to="Flight">항공권</NavLink></li>
 								<li class="navItem"><NavLink to="/Accommodation">숙박</NavLink></li>
 							</ul>
-						</li>
+						</li> */}
 						<li class="navItem" className={HeaderCSS.navItem} onMouseLeave={() => setDropdownOpen(false)}>
 							<a href="#" onClick={toggleDropdown}>여행</a>
 							{isDropdownOpen && (
@@ -172,6 +172,7 @@ function Header() {
 								<li>
 									<a href="#">여행지</a>
 								</li>
+								<li><NavLink to="Restaurants">맛집</NavLink></li>
 								<li>
 									<NavLink to="/Flight">항공권</NavLink>
 								</li>
