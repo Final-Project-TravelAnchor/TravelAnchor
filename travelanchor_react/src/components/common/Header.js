a43cae8227d905aa897fb26ae13a0e6badd11368
Jinny04 <cje364182@gmail.com>
2024-11-27 17:43:45 +0900

    맛집 스타일 수정

--- travelanchor_react/src/components/common/Header.js
+++ travelanchor_react/src/components/common/Header.js
@@ -8,7 +8,7 @@ import LoginModal from "./LoginModal";
 import commonCss from "./common.module.css";
 import { callGetMemberAPI, callGetPoint  } from '../../apis/MemberAPICalls';
 import { callTravelReportByMemberCodeAPI } from "../../apis/TravelReportAPICalls";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 function Header({ hideAuthLinks }) {
 	const navigate = useNavigate();
